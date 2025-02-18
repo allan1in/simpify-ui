22d6cc0b10b6250a1bc453b92182e64cf85612ce
allan1in <allanlinemail@gmail.com>
2025-02-18 16:56:49 +0800

    stash [skip ci]

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import type { ButtonProps } from './types';
 defineOptions({
-  name: 'SpButton'
-})
+  name: 'ErButton',
+});
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: 'button',
+  nativeType: 'button',
+});
+
+const slots = defineSlots();
+const _ref = ref<HTMLButtonElement>();
 </script>
+
 <template>
-  <button style="color: red">test button</button>
+  <component :is="props.tag" ref="_ref" class="er-button" :type="props.tag === 'button' ? nativeType : void 0"
+    :disabled="disabled || loading ? true : void 0" :class="[
+      `er-button-${type}`, type,
+      `er-button-${size}`, size,
+      { 'is-plain': plain, 'is-round': round, 'is-circle': circle, 'is-disabled': disabled, 'is-loading': loading }
+    ]">
+    <slot></slot>
+  </component>
 </template>
